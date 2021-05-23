722b0b41cddcb290f758038cd62a473e86d64d5c
Takehiro Sano <tsano430@gmail.com>
2021-05-24 00:31:43 +0900

    Refactoring

--- ClusterEnsembles/__init__.py
+++ ClusterEnsembles/__init__.py
@@ -1,6 +1,14 @@
 # coding: utf-8
 
+
+# __init__.py
+#   Author: Takehiro Sano
+#   Contact: tsano430@gmail.com
+#   License: MIT License
+
+
 from .ClusterEnsembles import cluster_ensembles
+
 
 __all__ = ['cluster_ensembles']
 
